3a222f9e0cca2f3ae4c53180ea9baa4fbc0a79af
min0k <mino.kim@minokim.com>
2022-01-03 14:43:41 -0800

    Pass props into card

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 // import logo from './logo.svg';
-import './App.css';
-import Navbar from './Components/Navbar';
-import Card from './Components/Card';
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Card from "./Components/Card";
+import dataArray from "./data";
 
 function App() {
+  const data = dataArray.map((items) => {
+    return <Card key={items.title} cardData={items} />;
+  });
+
   return (
     <div>
       <Navbar />
-      <Card />
-      <Card />
-      <Card />
-      </div>
+      {data}
+    </div>
   );
 }
 
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,27 +1,21 @@
 import pic from "./logo.png";
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props);
   return (
     <div className="card">
       <img src={pic} alt="" width="100px"></img>
-      <CardInfo />
+      <div>
+        <p>
+          📍{props.cardData.location} <span className="grey"><a href={props.cardData.googleMapsUrl}>View on Google maps</a></span>
+        </p>
+        <h1>{props.cardData.title}</h1>
+        <p>{props.cardData.startDate} - {props.cardData.endDate}</p>
+        <p>
+          {props.cardData.description}
+        </p>
+      </div>
     </div>
+    
   );
 }
-
-const CardInfo = () => {
-  return (
-    <div>
-      <p>
-        📍Japan <span className="grey">View on Google Maps</span>
-      </p>
-      <h1>Mount Fuji</h1>
-      <p>12 Jan, 2021 - 24 Jan, 2021.</p>
-      <p>
-        Mount Fuji is the tallest mountain in Japan, standing at 3776 feet tall,
-        Mount Fuji is the single most popular tourist site in Japan, for both
-        Japanese and foreign tourists.
-      </p>
-    </div>
-  );
-};
